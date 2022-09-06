a7b0bb9dc7f4320359d9c271fd24559ce0fd15ab
MuratYucedag <MurattYucedag@users.noreply.github.com>
2022-09-06 21:03:22 +0300

    Ders ortası

---
+++ Project_Personal_Demo/Controllers/DefaultController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Project_Personal_Demo.Models;
+
+namespace Project_Personal_Demo.Controllers
+{
+    public class DefaultController : Controller
+    {
+        PersonalDbEntities db = new PersonalDbEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult HeaderPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult NavbarPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult BannerPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult SkillPartial()
+        {
+            var values = db.TblSkill.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult FeaturePartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult ImagePartial()
+        {
+            var values = db.TblImage.ToList();
+            return PartialView(values);
+        }
+    }
+}
